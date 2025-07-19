892235071648f1bc00e26706ffef278c4841e2ef
Jorgezapata-02 <jorgedejesuspriegozapata@gmail.com>
2025-07-19 14:27:53 -0600

    Add files via upload

---
+++ pila.py
@@ -0,0 +1,17 @@
+class PilaApoyos:
+    def __init__(self):
+        self.items = []
+
+    def esta_vacia(self):
+        return len(self.items) == 0
+
+    def apilar(self, item):
+        self.items.append(item)
+
+    def desapilar(self):
+        if not self.esta_vacia():
+            return self.items.pop()
+        return None
+
+    def mostrar(self):
+        return list(reversed(self.items))  # Para mostrar de arriba hacia abajo
